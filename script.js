e89c8c7a9d83077ff930b98ed36c8443784289fd
guntoroyk <guntoro.gyk@gmail.com>
2019-04-02 07:45:12 +0700

    okee

--- script.js
+++ script.js
@@ -1,14 +1,39 @@
 
+var firstInput;
+var secondInput;
+var result;
+
+function ambilData(){
+    firstInput = document.querySelector("#inputOne").value;
+
+    secondInput = document.querySelector("#inputTwo").value;
+    result = document.querySelector("#result");
+}
+
 function tambah(){
-    var firstInput = document.querySelector("#inputOne").value;
-
-    var secondInput = document.querySelector("#inputTwo").value;
-    var result = document.querySelector("#result");
-
-    var sum = parseInt(firstInput) + parseInt(secondInput);
-    result.innerText = sum;
+    ambilData();
+    var hasil = parseInt(firstInput) + parseInt(secondInput);
+    result.innerText = hasil;
 }
 
 function kurang(){
+    ambilData();
+    var hasil = parseInt(firstInput) - parseInt(secondInput);
+    result.innerText = hasil;
+}
 
+function kali(){
+    ambilData();
+    var hasil = parseInt(firstInput) * parseInt(secondInput);
+    result.innerText = hasil;
+}
+function bagi(){
+    ambilData();
+    var hasil = parseInt(firstInput) / parseInt(secondInput);
+    result.innerText = hasil;
+}
+
+function hapus(){
+    document.querySelector("#calculator").reset();
+    result.innerText = "____";
 }
